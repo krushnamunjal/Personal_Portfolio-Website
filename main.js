52cf412cddacb77dda6df3b1fd48582dbd288bf1
krushnamunjal <152583521+krushnamunjal@users.noreply.github.com>
2024-09-25 00:16:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Software Developer", "Web Developer"], // The strings that will be typed
+    typeSpeed: 100,  // Speed at which the text is typed (in milliseconds)
+    backSpeed: 100,  // Speed at which the text is deleted (in milliseconds)
+    backDelay: 1000, // Delay before starting to delete (in milliseconds)
+    loop: true       // Whether to loop the animation or not
+});
+
